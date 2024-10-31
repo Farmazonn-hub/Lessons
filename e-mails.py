6a4cc4c18beb0b5971277e248b41cbe819a61b45
Farmazonn-hub <neprukhin@yandex.ru>
2024-11-01 02:20:38 +0400

    Update e-mails.py

--- e-mails.py
+++ e-mails.py
@@ -1,8 +1,8 @@
 def  send_email( message, recipient,*, sender = "university.help@gmail.com"):
     if sender == recipient:
         print("Нельзя отправить письмо самому себе!")
-    elif '@'  not in recipient or not recipient.endswith(
-            ('.com', '.ru', '.net')) or '@' not in sender or not sender.endswith(('.com', '.ru', '.net')) :
+    elif ('@' not in recipient or not recipient.endswith(('.com', '.ru', '.net'))
+            or '@' not in sender or not sender.endswith(('.com', '.ru', '.net'))) :
         print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
     elif sender != "university.help@gmail.com":
         print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}")
