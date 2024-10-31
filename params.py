2ebe96abec2ec1aec206a105a0e9a8ccdb087134
Farmazonn-hub <neprukhin@yandex.ru>
2024-11-01 02:45:59 +0400

    Add files via upload

---
+++ params.py
@@ -0,0 +1,13 @@
+def print_params(a = 1, b = 'строка', c = True):
+    print(a, b, c)
+
+values_list = ['yep', 2, True]
+values_dict = {'a': 'no', 'b': 4, 'c': False}
+values_list_2 = [54.32, 'Строка' ]
+
+print_params()
+print_params(b = 25)
+print_params(c = [1,2,3])
+print_params(*values_list)
+print_params(**values_dict)
+print_params(*values_list_2, 42)
